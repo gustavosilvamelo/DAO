40adaa45f0a9d52d7c3ad1e180a7dbb637dc04cd
Gustavo Silva Melo <gustavosilvamelo91@gmail.com>
2017-09-27 18:46:32 -0300

    Seção 13 - Aula 63

---
+++ class/usuario.php
@@ -0,0 +1,63 @@
+<?php
+
+class Usuario {
+
+	private $idusuario;
+	private $deslogin;
+	private $dessenha;
+	private $dtcadastro;
+
+	public function getIdusuario(){
+		return $this->idusuario;
+	}
+
+	public function getDeslogin(){
+		return $this->deslogin;
+	}
+
+	public function getDessenha(){
+		return $this->dessenha;
+	}
+
+	public function getDtcadastro(){
+		return $this->dtcadastro;
+	}
+
+	public function setIdusuario($value){
+		$this->idusuario = $value;
+	}
+
+	public function setDeslogin($value){
+		$this->deslogin = $value;
+	}
+
+	public function setDessenha($value){
+		$this->dessenha = $value;
+	}
+
+	public function setDtcadastro($value){
+		$this->dtcadastro = $value;
+	}
+
+	public function loadById($id){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(: :ID=>$id
+		));
+	}
+
+	if (count($results) > 0 ) {
+
+		$row = $results[0];
+
+		$this->setIdusuario($row['idusuario']);
+		$this->setDeslogin($row['deslogin']);
+		$this->dessenha($row['dessenha']);
+		$this->dtcadastro(new DateTime($row['dtcadastro']));
+
+	}
+
+}
+
+?>
